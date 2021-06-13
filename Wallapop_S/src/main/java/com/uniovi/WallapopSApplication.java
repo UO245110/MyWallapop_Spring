93517e03bd30abe5a5dd12ba3a7ae61bacae4e23
Xurde <uo245110@uniovi.es>
2021-06-13 17:11:17 +0200

    Aquí comienza la travesía

---
+++ Wallapop_S/src/main/java/com/uniovi/WallapopSApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallapopSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallapopSApplication.class, args);
+	}
+
+}
